2e856a77cbc24617cea378e5f1bb23c29db059fc
Hukeng <65363660+HukengArt@users.noreply.github.com>
2020-05-27 20:25:57 +0200

    Delete hukMath.js

--- hukMath.js
+++
@@ -1,5 +0,0 @@
-
-
-export function vecAngle2D(vec0, vec1) {
-    return Math.acos((vec0[0] * vec1[0] + vec0[1] * vec1[1]) / (Math.sqrt(Math.pow(vec0[0], 2) + Math.pow(vec0[1], 2)) * Math.sqrt(Math.pow(vec1[0], 2) + Math.pow(vec1[1], 2))));
-  }
